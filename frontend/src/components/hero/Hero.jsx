5cb100dea6fb3d8346f10a1728ac457f3ac6858c
islam solaiman <islam.solaiman92@gmail.com>
2024-03-25 00:13:38 +0400

    Front End Update

--- frontend/src/components/hero/Hero.jsx
+++ frontend/src/components/hero/Hero.jsx
@@ -123,8 +123,131 @@ const Hero = () => {
           })}
         </Swiper>
 
-        <IconSection />
+        <Box sx={{ display: { xs: "none", md: "block", minWidth: "26.6%" } }}>
+          <Box sx={{ position: "relative" }}>
+            <img width={"100%"} src="src/images/banner-17.jpg" alt="" />
+
+            <Stack
+              sx={{
+                position: "absolute",
+                top: "50%",
+                transform: "translateY(-50%)",
+                left: 31,
+              }}
+            >
+              <Typography
+                variant="caption"
+                sx={{
+                  color: "#2B3445",
+                  fontSize: "18px",
+                }}
+              >
+                NEW ARRIVALS
+              </Typography>
+              <Typography
+                variant="h6"
+                sx={{
+                  color: "#2B3445",
+                  lineHeight: "16px",
+                  mt: 1,
+                }}
+              >
+                SUMMER
+              </Typography>
+              <Typography
+                variant="h6"
+                sx={{
+                  color: "#2B3445",
+                }}
+              >
+                SALE 20% OFF
+              </Typography>
+
+              <Link
+                sx={{
+                  color: "#2B3445",
+                  display: "flex",
+                  alignItems: "center",
+                  gap: "5px",
+                  transition: "0.2s",
+
+                  "&:hover": {
+                    color: "#D23F57",
+                  },
+                }}
+                href="#"
+                underline="none"
+              >
+                shop now
+                <ArrowForwardIcon sx={{ fontSize: "13px" }} />
+              </Link>
+            </Stack>
+          </Box>
+
+          <Box sx={{ position: "relative" }}>
+            <img width={"100%"} src="src/images/banner-16.jpg" alt="" />
+            <Stack
+              sx={{
+                position: "absolute",
+                top: "50%",
+                transform: "translateY(-50%)",
+                left: 31,
+              }}
+            >
+              <Typography
+                variant="caption"
+                sx={{
+                  color: "#2B3445",
+                  fontSize: "18px",
+                  fontWeight: 300,
+                }}
+              >
+                GAMING 4K
+              </Typography>
+              <Typography
+                variant="h6"
+                sx={{
+                  color: "#2B3445",
+                  lineHeight: "16px",
+                  mt: 1,
+                }}
+              >
+                DESKTOPS &
+              </Typography>
+
+              <Typography
+                variant="h6"
+                sx={{
+                  color: "#2B3445",
+                }}
+              >
+                LAPTOPS
+              </Typography>
+
+              <Link
+                sx={{
+                  color: "#2B3445",
+                  display: "flex",
+                  alignItems: "center",
+                  gap: "5px",
+                  transition: "0.2s",
+
+                  "&:hover": {
+                    color: "#D23F57",
+                  },
+                }}
+                href="#"
+                underline="none"
+              >
+                shop now
+                <ArrowForwardIcon sx={{ fontSize: "13px" }} />
+              </Link>
+            </Stack>
+          </Box>
+        </Box>
       </Box>
+
+      <IconSection />
     </Container>
   );
 };
